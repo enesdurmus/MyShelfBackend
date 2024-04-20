5b4f0518b15c0199df0296b2ccdc98ec2a89e05c
Enes Durmuş <enes.d@passionpunch.studio>
2024-04-20 16:07:30 +0300

    user entity created

---
+++ my-shelf-backend/src/main/java/com/enesd/myshelfbackend/model/entity/User.java
@@ -0,0 +1,63 @@
+package com.enesd.myshelfbackend.model.entity;
+
+import com.enesd.myshelfbackend.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+public class User implements UserDetails {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", nullable = false))
+    @Column(name = "role_type")
+    private Set<RoleType> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
