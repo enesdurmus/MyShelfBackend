11c23276b4cd312721fa4e6f517b4e0816838ff1
Enes Durmuş <enes.d@passionpunch.studio>
2024-04-20 18:32:47 +0300

    controller changed to rest controller

--- my-shelf-backend/src/main/java/com/enesd/myshelfbackend/controller/AuthController.java
+++ my-shelf-backend/src/main/java/com/enesd/myshelfbackend/controller/AuthController.java
@@ -5,13 +5,13 @@ import com.enesd.myshelfbackend.model.request.SignUpRequest;
 import com.enesd.myshelfbackend.model.response.GenericResponse;
 import com.enesd.myshelfbackend.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
 public class AuthController {
