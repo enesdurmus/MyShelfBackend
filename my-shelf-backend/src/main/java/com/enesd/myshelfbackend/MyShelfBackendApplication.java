f1775cffbe7eb7f348d226b5545d39b3aff327ea
Enes Durmuş <enes.d@passionpunch.studio>
2024-04-20 15:42:34 +0300

    feat: init project

---
+++ my-shelf-backend/src/main/java/com/enesd/myshelfbackend/MyShelfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enesd.myshelfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShelfBackendApplication.class, args);
+	}
+
+}
---
+++ my-shelf-backend/src/test/java/com/enesd/myshelfbackend/MyShelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesd.myshelfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
