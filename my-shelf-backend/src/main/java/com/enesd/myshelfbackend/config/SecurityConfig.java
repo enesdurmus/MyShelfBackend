76fc09f72934c1dd5c49190f1e1bb2fd76aeb05f
Enes Durmuş <enes.d@passionpunch.studio>
2024-04-20 16:25:55 +0300

    security filter chain implemented

--- my-shelf-backend/src/main/java/com/enesd/myshelfbackend/config/SecurityConfig.java
+++ my-shelf-backend/src/main/java/com/enesd/myshelfbackend/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.enesd.myshelfbackend.config;
 
+import com.enesd.myshelfbackend.enums.RoleType;
+import com.enesd.myshelfbackend.filters.JwtAuthorizationFilter;
 import com.enesd.myshelfbackend.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +11,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +27,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfig {
 
     private final UserRepository userRepository;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    private AuthenticationProvider authenticationProvider() {
+    public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
@@ -44,4 +53,31 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(
+                        (request, response, exception) -> {
+                            System.out.println(exception);
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+                        }))
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/api/*/auth/**")
+                                .permitAll()
+                                .requestMatchers("/api/*/*/admin/**")
+                                .hasAuthority(RoleType.ROLE_ADMIN.toString())
+                                .requestMatchers("/api/*/*/user/**")
+                                .hasAnyAuthority(RoleType.ROLE_ADMIN.toString(), RoleType.ROLE_USER.toString())
+                                .anyRequest().authenticated())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authenticationProvider(authenticationProvider)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
 }
