0283f54950fdcabaa56010820de59a335df7dbce
Enes Durmuş <enes.d@passionpunch.studio>
2024-04-20 18:20:39 +0300

    auth controller signup implemented

--- my-shelf-backend/src/main/java/com/enesd/myshelfbackend/controller/AuthController.java
+++ my-shelf-backend/src/main/java/com/enesd/myshelfbackend/controller/AuthController.java
@@ -1,9 +1,26 @@
 package com.enesd.myshelfbackend.controller;
 
+import com.enesd.myshelfbackend.dto.SignInDTO;
+import com.enesd.myshelfbackend.model.request.SignUpRequest;
+import com.enesd.myshelfbackend.model.response.GenericResponse;
+import com.enesd.myshelfbackend.service.AuthService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
 
+    @PostMapping("/user/signup")
+    public GenericResponse signUp(@Validated @RequestBody SignUpRequest signUpRequest) {
+        SignInDTO signInDTO = authService.signUp(signUpRequest);
+        return GenericResponse.success(signInDTO);
+    }
 }
