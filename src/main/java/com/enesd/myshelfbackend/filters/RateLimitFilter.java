5859faa2f4c52cd8bc83167c2bda36810c6cd0af
Enes Durmuş <enes.d@passionpunch.studio>
2024-04-23 17:49:46 +0300

    RateLimitFilter&RateLimitService implemented

---
+++ src/main/java/com/enesd/myshelfbackend/filters/RateLimitFilter.java
@@ -0,0 +1,24 @@
+package com.enesd.myshelfbackend.filters;
+
+import com.enesd.myshelfbackend.service.RateLimitService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    private final RateLimitService rateLimitService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String ipAddress = request.getRemoteAddr();
+        rateLimitService.checkRateLimit(ipAddress);
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/enesd/myshelfbackend/service/RateLimitService.java
@@ -0,0 +1,67 @@
+package com.enesd.myshelfbackend.service;
+
+import com.enesd.myshelfbackend.consts.CacheNames;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class RateLimitService {
+    private static final int MAX_REQUESTS_PER_MINUTE = 100;
+    private static final int RATE_LIMIT_WINDOW_SECONDS = 60;
+    private static final String RATE_LIMIT_EXPIRE_POST_FIX = "::expires";
+
+    private static Logger log = LoggerFactory.getLogger(RateLimitService.class);
+
+    private final RedisTemplate<Object, Object> redisTemplate;
+
+    public void deleteExpiredRateLimits() {
+        try {
+            Instant currentUtc = Instant.now();
+            ArrayList<String> expiredKeys = new ArrayList<>();
+
+            ScanOptions options = ScanOptions.scanOptions().match("*" + RATE_LIMIT_EXPIRE_POST_FIX).build();
+            Cursor<Map.Entry<Object, Object>> cursor = redisTemplate.opsForHash().scan(CacheNames.RATE_LIMIT_HASH_KEY, options);
+
+            while (cursor.hasNext()) {
+                Map.Entry<Object, Object> entry = cursor.next();
+                Instant expiresAt = Instant.parse(entry.getValue().toString());
+                if (currentUtc.isAfter(expiresAt)) {
+                    expiredKeys.add(entry.getKey().toString());
+                    expiredKeys.add(entry.getKey().toString().split("::")[0]);
+                }
+            }
+
+            cursor.close();
+
+            if (expiredKeys.size() > 0) {
+                redisTemplate.opsForHash().delete(CacheNames.RATE_LIMIT_HASH_KEY, expiredKeys.toArray());
+            }
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+        }
+    }
+
+    public void checkRateLimit(String ipAddress) {
+        Long currentCount = redisTemplate.opsForHash().increment(CacheNames.RATE_LIMIT_HASH_KEY, ipAddress, 1);
+
+        if (currentCount == 1) {
+            redisTemplate.opsForHash().put(CacheNames.RATE_LIMIT_HASH_KEY, ipAddress +
+                    RATE_LIMIT_EXPIRE_POST_FIX, Instant.now().plus(Duration.ofSeconds(RATE_LIMIT_WINDOW_SECONDS)).toString());
+        }
+
+        if (currentCount > MAX_REQUESTS_PER_MINUTE) {
+            // throw new TooManyRequestException();
+        }
+    }
+}
