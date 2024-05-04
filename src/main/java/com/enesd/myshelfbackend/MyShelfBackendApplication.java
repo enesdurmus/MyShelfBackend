95e3ec5ac83aac2d66ab4eeea937fcca53a01a62
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-04 15:23:41 +0300

    repository packages specified

--- src/main/java/com/enesd/myshelfbackend/MyShelfBackendApplication.java
+++ src/main/java/com/enesd/myshelfbackend/MyShelfBackendApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@EnableElasticsearchRepositories(basePackages = "com.enesd.myshelfbackend.repository.elasticsearch")
+@EnableJpaRepositories(basePackages = "com.enesd.myshelfbackend.repository.jpa")
 @EnableScheduling
 public class MyShelfBackendApplication {
 
