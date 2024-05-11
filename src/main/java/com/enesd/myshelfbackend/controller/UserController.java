c8c285f4bd0978a61698b18c006b8dc67def6a52
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-11 16:29:07 +0300

    userController created

---
+++ src/main/java/com/enesd/myshelfbackend/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.enesd.myshelfbackend.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/user")
+@AllArgsConstructor
+public class UserController {
+}
