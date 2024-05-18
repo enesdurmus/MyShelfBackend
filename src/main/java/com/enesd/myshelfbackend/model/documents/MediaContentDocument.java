11ff988a017f45b57aec33e9b845c32511039f44
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-18 17:27:05 +0300

    mediaContentDocument field annotation added

--- src/main/java/com/enesd/myshelfbackend/model/documents/MediaContentDocument.java
+++ src/main/java/com/enesd/myshelfbackend/model/documents/MediaContentDocument.java
@@ -1,8 +1,19 @@
 package com.enesd.myshelfbackend.model.documents;
 
 import com.enesd.myshelfbackend.model.abstracts.MediaContent;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.Instant;
 
 @Document(indexName = "media_contents")
 public class MediaContentDocument extends MediaContent {
+
+    @Override
+    @Field(type = FieldType.Date, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSS")
+    public Instant getReleaseDate() {
+        return super.getReleaseDate();
+    }
 }
