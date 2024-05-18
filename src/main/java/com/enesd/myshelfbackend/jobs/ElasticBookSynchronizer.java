4c14196cb5dd8032c74f34563b2f7e005471b824
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-18 17:28:43 +0300

    synchronizeMediaContentWithElastic job implemented

--- src/main/java/com/enesd/myshelfbackend/jobs/ElasticBookSynchronizer.java
+++ src/main/java/com/enesd/myshelfbackend/jobs/ElasticBookSynchronizer.java
@@ -1,9 +1,14 @@
 package com.enesd.myshelfbackend.jobs;
 
+import com.enesd.myshelfbackend.model.abstracts.MediaContent;
 import com.enesd.myshelfbackend.model.documents.BookDocument;
+import com.enesd.myshelfbackend.model.documents.MediaContentDocument;
 import com.enesd.myshelfbackend.model.entities.BookEntity;
+import com.enesd.myshelfbackend.model.entities.MediaContentEntity;
 import com.enesd.myshelfbackend.repository.elasticsearch.BookDocumentRepository;
+import com.enesd.myshelfbackend.repository.elasticsearch.MediaContentDocumentRepository;
 import com.enesd.myshelfbackend.repository.jpa.BookEntityRepository;
+import com.enesd.myshelfbackend.repository.jpa.MediaContentEntityRepository;
 import lombok.AllArgsConstructor;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.modelmapper.ModelMapper;
@@ -23,9 +28,11 @@ public class ElasticBookSynchronizer {
     private static final Logger logger = LoggerFactory.getLogger(ElasticBookSynchronizer.class);
     private final BookDocumentRepository bookDocumentRepository;
     private final BookEntityRepository bookEntityRepository;
+    private final MediaContentDocumentRepository mediaContentDocumentRepository;
+    private final MediaContentEntityRepository mediaContentEntityRepository;
     private final ModelMapper modelMapper;
 
-    @Scheduled(cron = "* * * * 1 *")
+    @Scheduled(cron = "0 0 0 * * SAT")
     @SchedulerLock(name = "ElasticBookSynchronizer.synchronizeBooksWithRDBM", lockAtLeastFor = "PT15S", lockAtMostFor = "PT30S")
     public void synchronizeBooksWithRDBM() {
         List<Integer> elasticBookIds = bookDocumentRepository.findAllIds();
@@ -48,6 +55,38 @@ public class ElasticBookSynchronizer {
                         .map(user -> modelMapper.map(user, BookDocument.class)).toList();
 
                 bookDocumentRepository.saveAll(bookDocuments);
+                pageNumber++;
+                logger.info("Page is not empty continue to sync operation! Page number:{}", pageNumber);
+            } else {
+                isSynchronizing = false;
+                logger.info("Page is empty finish sync operation! Page number:{}", pageNumber);
+            }
+        } while (isSynchronizing);
+    }
+
+    @Scheduled(cron = "0 0 0 * * FRI")
+    @SchedulerLock(name = "ElasticBookSynchronizer.synchronizeMediaContentsWithRDBM", lockAtLeastFor = "PT15S", lockAtMostFor = "PT30S")
+    public void synchronizeMediaContentsWithRDBM() {
+        List<Integer> elasticMediaContentIds = mediaContentDocumentRepository.findAllIds();
+
+        if (elasticMediaContentIds.isEmpty()) {
+            logger.info("There is no entry in ES add dummy value");
+            elasticMediaContentIds.add(0);
+        }
+
+        boolean isSynchronizing = true;
+        int pageNumber = 0;
+
+        do {
+            Pageable pageable = PageRequest.of(pageNumber, 1000, Sort.by("id"));
+            List<MediaContentEntity> mediaContentEntities = mediaContentEntityRepository.findByIdNotIn(elasticMediaContentIds, pageable);
+
+            if (!elasticMediaContentIds.isEmpty()) {
+                List<MediaContentDocument> mediaContentDocuments = mediaContentEntities
+                        .stream()
+                        .map(user -> modelMapper.map(user, MediaContentDocument.class)).toList();
+
+                mediaContentDocumentRepository.saveAll(mediaContentDocuments);
                 pageNumber++;
                 logger.info("Page is not empty continue to sync operation! Page number:{}", pageNumber);
             } else {
