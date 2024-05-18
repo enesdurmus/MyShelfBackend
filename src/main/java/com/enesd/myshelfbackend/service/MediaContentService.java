33e0257c968b804470e49a99e12c8708c953bb2d
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-18 17:27:37 +0300

    mediaContentService implemented

---
+++ src/main/java/com/enesd/myshelfbackend/service/MediaContentService.java
@@ -0,0 +1,30 @@
+package com.enesd.myshelfbackend.service;
+
+import com.enesd.myshelfbackend.model.abstracts.MediaContent;
+import com.enesd.myshelfbackend.model.documents.BookDocument;
+import com.enesd.myshelfbackend.model.documents.MediaContentDocument;
+import com.enesd.myshelfbackend.model.entities.BookEntity;
+import com.enesd.myshelfbackend.model.entities.MediaContentEntity;
+import com.enesd.myshelfbackend.repository.elasticsearch.MediaContentDocumentRepository;
+import com.enesd.myshelfbackend.repository.jpa.MediaContentEntityRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class MediaContentService {
+
+    private final MediaContentEntityRepository mediaContentEntityRepository;
+    private final MediaContentDocumentRepository mediaContentDocumentRepository;
+
+    public List<MediaContentDocument> searchRelatedMediaContents(String searchTerm) {
+        return mediaContentDocumentRepository.findWithSearchTerm(searchTerm);
+    }
+
+    public MediaContentEntity findMediaContentById(int mediaContentId) {
+        return mediaContentEntityRepository.findById(mediaContentId).orElseThrow(EntityNotFoundException::new);
+    }
+}
