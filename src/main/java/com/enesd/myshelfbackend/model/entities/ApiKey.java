67a121b2e7c41de46a665a51f89a676275bfc936
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-25 15:33:03 +0300

    ApiKey model created

--- src/main/java/com/enesd/myshelfbackend/model/entities/ApiKey.java
+++ src/main/java/com/enesd/myshelfbackend/model/entities/ApiKey.java
@@ -1,4 +1,25 @@
 package com.enesd.myshelfbackend.model.entities;
 
-public class ApiKey {
+import com.enesd.myshelfbackend.model.abstracts.Auditable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "api_keys")
+@AllArgsConstructor
+public class ApiKey extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long apiKeyId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "api_key", nullable = false, unique = true)
+    private String apiKey;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
 }
