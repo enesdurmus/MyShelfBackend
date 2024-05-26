3bfe78832f551a6dd34a17d0c89438d117d19837
Enes Durmuş <enes.d@passionpunch.studio>
2024-05-26 23:41:05 +0300

    AccessToken model added

---
+++ src/main/java/com/enesd/myshelfbackend/model/entities/AccessToken.java
@@ -0,0 +1,18 @@
+package com.enesd.myshelfbackend.model.entities;
+
+import com.enesd.myshelfbackend.model.abstracts.Auditable;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "access_tokens")
+public class AccessToken extends Auditable {
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    @Column(name = "access_token", unique = true, columnDefinition = "TEXT")
+    private String accessToken;
+}
