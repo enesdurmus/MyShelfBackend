4209d064af1278f85db839f0a7ea61f839f178ce
Enes Durmuş <enes.d@passionpunch.studio>
2024-06-01 20:25:14 +0300

    CollectionDTO implemented

--- src/main/java/com/enesd/myshelfbackend/dto/CollectionDTO.java
+++ src/main/java/com/enesd/myshelfbackend/dto/CollectionDTO.java
@@ -1,4 +1,16 @@
 package com.enesd.myshelfbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CollectionDTO {
+    private Long id;
+    private String name;
+    private UUID userId;
 }
