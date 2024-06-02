c923f3280f1c710060197d217274f2b28c0593a3
Enes Durmuş <enes.d@passionpunch.studio>
2024-06-03 00:45:57 +0300

    BookDTO added

---
+++ src/main/java/com/enesd/myshelfbackend/dto/BookDTO.java
@@ -0,0 +1,14 @@
+package com.enesd.myshelfbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDTO {
+    private Long id;
+    private String isbn;
+    private String title;
+}
