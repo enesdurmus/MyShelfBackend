f3b38d6ad8f7bd5af82d6f5d7be6b2a256671ebb
Enes Durmuş <enes.d@passionpunch.studio>
2024-06-03 16:51:20 +0300

    DatabaseSeeder class implemented

--- src/main/java/com/enesd/myshelfbackend/dto/BookDTO.java
+++ src/main/java/com/enesd/myshelfbackend/dto/BookDTO.java
@@ -1,5 +1,6 @@
 package com.enesd.myshelfbackend.dto;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,11 @@ public class BookDTO {
     private Long id;
     private String isbn;
     private String title;
+    private String titleWithoutSeries;
+    private String countryCode;
+    private Float averageRating;
+    private int pageNumber;
+    private String description;
+    private String publisher;
+    private String imageUrl;
 }
--- src/main/java/com/enesd/myshelfbackend/seeders/DatabaseSeeder.java
+++ src/main/java/com/enesd/myshelfbackend/seeders/DatabaseSeeder.java
@@ -1,2 +1,83 @@
-package com.enesd.myshelfbackend.seeders;public class DatabaseSeeder {
+package com.enesd.myshelfbackend.seeders;
+
+import com.enesd.myshelfbackend.model.entities.BookEntity;
+import com.enesd.myshelfbackend.model.entities.MediaContentEntity;
+import com.enesd.myshelfbackend.repository.jpa.BookEntityRepository;
+import com.enesd.myshelfbackend.repository.jpa.MediaContentEntityRepository;
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+public class DatabaseSeeder {
+
+    private BookEntityRepository bookEntityRepository;
+    private MediaContentEntityRepository mediaContentEntityRepository;
+
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        seedBookTable();
+        seedMediaContentTable();
+    }
+
+    private void seedBookTable() {
+        Faker faker = new Faker();
+        class BookCreator {
+            private final Faker faker;
+
+            public BookCreator(Faker faker) {
+                this.faker = faker;
+            }
+
+            public BookEntity create() {
+                BookEntity bookEntity = new BookEntity();
+                bookEntity.setTitle(faker.book().title());
+                bookEntity.setAverageRating((float) faker.number().randomDouble(2, 1, 100));
+                bookEntity.setCountryCode(faker.country().countryCode2());
+                bookEntity.setDescription(faker.gameOfThrones().character());
+                bookEntity.setPageNumber(faker.number().randomDigit());
+                return bookEntity;
+            }
+        }
+
+        BookCreator bookCreator = new BookCreator(faker);
+        for (int i = 0; i < 20; i++) {
+            BookEntity book = bookCreator.create();
+            bookEntityRepository.save(book);
+        }
+    }
+
+    private void seedMediaContentTable() {
+        Faker faker = new Faker();
+        class MediaContentCreator {
+            private final Faker faker;
+
+            public MediaContentCreator(Faker faker) {
+                this.faker = faker;
+            }
+
+            public MediaContentEntity create() {
+                MediaContentEntity mediaContentEntity = new MediaContentEntity();
+                mediaContentEntity.setTitle(faker.book().title());
+                mediaContentEntity.setAdult(faker.bool().bool());
+                mediaContentEntity.setReleaseDate(faker.date().birthday().toInstant());
+                mediaContentEntity.setRevenue((long) faker.number().numberBetween(100, 100000000));
+                mediaContentEntity.setVoteAverage((float) faker.number().randomDouble(10, 10, 1000));
+                mediaContentEntity.setRuntime(faker.number().randomDigit());
+                mediaContentEntity.setVoteCount(faker.number().randomDigit());
+                return mediaContentEntity;
+            }
+        }
+
+        MediaContentCreator mediaContentCreator = new MediaContentCreator(faker);
+        for (int i = 0; i < 20; i++) {
+            MediaContentEntity mediaContentEntity = mediaContentCreator.create();
+            mediaContentEntityRepository.save(mediaContentEntity);
+        }
+    }
 }
