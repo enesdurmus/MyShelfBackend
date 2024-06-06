ae19fc38a9c60cadf6bdbfaea4c0aa52db9b6eaf
Enes Durmuş <enes.d@passionpunch.studio>
2024-06-06 12:53:21 +0300

    ElasticMediaContentSync job implemented

---
+++ src/main/java/com/enesd/myshelfbackend/jobs/ElasticMediaContentSync.java
@@ -0,0 +1,80 @@
+package com.enesd.myshelfbackend.jobs;
+
+import com.enesd.myshelfbackend.model.abstracts.Book;
+import com.enesd.myshelfbackend.model.abstracts.MediaContent;
+import com.enesd.myshelfbackend.model.documents.BookDocument;
+import com.enesd.myshelfbackend.model.documents.MediaContentDocument;
+import com.enesd.myshelfbackend.model.entities.BookEntity;
+import com.enesd.myshelfbackend.model.entities.MediaContentEntity;
+import com.enesd.myshelfbackend.model.entities.SyncStatus;
+import com.enesd.myshelfbackend.repository.elasticsearch.MediaContentDocumentRepository;
+import com.enesd.myshelfbackend.repository.jpa.MediaContentEntityRepository;
+import com.enesd.myshelfbackend.repository.jpa.SyncStatusRepository;
+import com.enesd.myshelfbackend.utils.CustomModelMapper;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class ElasticMediaContentSync {
+    private final Logger logger = LoggerFactory.getLogger(ElasticMediaContentSync.class);
+    private final MediaContentEntityRepository mediaContentEntityRepository;
+    private final MediaContentDocumentRepository mediaContentDocumentRepository;
+    private final SyncStatusRepository syncStatusRepository;
+    private final CustomModelMapper modelMapper;
+
+    @Transactional
+    @Scheduled(fixedRateString = "60000")
+    public void syncMediaContents() {
+        try {
+            SyncStatus syncStatus = syncStatusRepository.findByEntityName(MediaContent.class.getName())
+                    .orElse(null);
+
+            Instant lastSyncTime;
+            Instant now = Instant.now();
+            boolean isInitialSync = (syncStatus == null);
+            boolean isSynchronizing = true;
+            int pageNumber = 0;
+            lastSyncTime = isInitialSync ? Instant.ofEpochSecond(0) : syncStatus.getLastSyncTime();
+
+            do {
+                Pageable pageable = PageRequest.of(pageNumber, 100, Sort.by("updatedAt"));
+                List<MediaContentEntity> mediaContentEntities = mediaContentEntityRepository.findAllByUpdatedAtIsGreaterThanEqual(lastSyncTime, pageable);
+                if (!mediaContentEntities.isEmpty()) {
+                    List<MediaContentDocument> mediaContentDocuments = mediaContentEntities
+                            .stream()
+                            .map(book -> modelMapper.map(book, MediaContentDocument.class)).toList();
+
+                    mediaContentDocumentRepository.saveAll(mediaContentDocuments);
+                    pageNumber++;
+                    logger.info("Page is not empty continue to sync operation! Current page:{}", pageNumber);
+                } else {
+                    isSynchronizing = false;
+                    logger.info("Page is empty finish sync operation! Current page:{}", pageNumber);
+                }
+            } while (isSynchronizing);
+
+            if (isInitialSync) {
+                syncStatus = new SyncStatus();
+                syncStatus.setEntityName(MediaContent.class.getName());
+            }
+
+            syncStatus.setLastSyncTime(now);
+            syncStatusRepository.save(syncStatus);
+
+            logger.info("Sync for {} completed successfully at {}", MediaContent.class.getName(), now);
+        } catch (Exception e) {
+            logger.error("Error during sync for entity: " + MediaContent.class.getName(), e);
+        }
+    }
+}
--- src/main/java/com/enesd/myshelfbackend/repository/jpa/MediaContentEntityRepository.java
+++ src/main/java/com/enesd/myshelfbackend/repository/jpa/MediaContentEntityRepository.java
@@ -4,8 +4,9 @@ import com.enesd.myshelfbackend.model.entities.MediaContentEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface MediaContentEntityRepository extends JpaRepository<MediaContentEntity, Long> {
-    List<MediaContentEntity> findByIdNotIn(List<Long> mediaContentIds, Pageable pageable);
+    List<MediaContentEntity> findAllByUpdatedAtIsGreaterThanEqual(Instant date, Pageable pageable);
 }
