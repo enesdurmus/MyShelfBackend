5df03bfa97a589e7048276e364484c3a741bf342
Enes Durmuş <enesdurmus3738@gmail.com>
2024-09-16 17:55:58 +0300

    rabbitMq converter allow list configured

--- src/main/java/com/enesd/myshelfbackend/config/RabbitMQConfig.java
+++ src/main/java/com/enesd/myshelfbackend/config/RabbitMQConfig.java
@@ -2,13 +2,23 @@ package com.enesd.myshelfbackend.config;
 
 import com.enesd.myshelfbackend.consts.RabbitMQQueueNames;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class RabbitMQConfig {
     @Bean
     public Queue displayNameSyncQueue() {
         return new Queue(RabbitMQQueueNames.DISPLAY_NAME_SYNC_QUEUE, false);
+    }
+
+    @Bean
+    public SimpleMessageConverter converter() {
+        SimpleMessageConverter converter = new SimpleMessageConverter();
+        converter.setAllowedListPatterns(List.of("com.enesd.myshelfbackend.*", "java.util.*", "java.lang.*", "org.hibernate.*"));
+        return converter;
     }
 }
