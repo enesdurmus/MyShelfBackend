2f943e00e799676a6225ddeedd67299c4a43a37e
Enes Durmuş <enesdurmus3738@gmail.com>
2024-09-16 17:56:30 +0300

    rabbitmq messageConsume&Produce methods implemented for displayNameSync

--- src/main/java/com/enesd/myshelfbackend/messaging/MessageConsumer.java
+++ src/main/java/com/enesd/myshelfbackend/messaging/MessageConsumer.java
@@ -1,14 +1,19 @@
 package com.enesd.myshelfbackend.messaging;
 
 import com.enesd.myshelfbackend.consts.RabbitMQQueueNames;
+import com.enesd.myshelfbackend.model.entities.User;
+import com.enesd.myshelfbackend.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class MessageConsumer {
+    private final UserService userService;
 
     @RabbitListener(queues = RabbitMQQueueNames.DISPLAY_NAME_SYNC_QUEUE)
-    public void receiveDisplayNameSyncMessage(String message) {
-        System.out.println("Received from firstQueue: " + message);
+    public void receiveDisplayNameSyncMessage(User user) {
+        userService.syncDisplayNameWithElastic(user);
     }
 }
--- src/main/java/com/enesd/myshelfbackend/messaging/MessageProducer.java
+++ src/main/java/com/enesd/myshelfbackend/messaging/MessageProducer.java
@@ -1,6 +1,7 @@
 package com.enesd.myshelfbackend.messaging;
 
 import com.enesd.myshelfbackend.consts.RabbitMQQueueNames;
+import com.enesd.myshelfbackend.model.entities.User;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class MessageProducer {
     private final RabbitTemplate rabbitTemplate;
 
-    public void sendDisplayNameSyncMessage() {
-        rabbitTemplate.convertAndSend(RabbitMQQueueNames.DISPLAY_NAME_SYNC_QUEUE, "sa");
+    public void sendDisplayNameSyncMessage(User user) {
+        rabbitTemplate.convertAndSend(RabbitMQQueueNames.DISPLAY_NAME_SYNC_QUEUE, user);
     }
 }
